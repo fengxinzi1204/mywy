e830e4e76deafe7d42343f24862b4bd1d5eadd98
fengxinzi1204 <2761950460@qq.com>
2019-09-05 11:02:30 +0800

    Scan The project according to the coding protocol and fix it

--- src/main/java/com/mzd/mywy/myshiro/login/DoMatch.java
+++ src/main/java/com/mzd/mywy/myshiro/login/DoMatch.java
@@ -3,25 +3,13 @@ package com.mzd.mywy.myshiro.login;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * @author Theodore
+ * @since 2019/9/5 11:00
+ */
 public class DoMatch {
 
-    char hexDigits[] = {
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd',
-            'e', 'f'};
-
-    /**
-     * 不加密
-     *
-     * @param password
-     * @param salt
-     * @param time
-     * @return
-     */
-    public String Unencrypted(String password, String salt, int time) {
-        String returnpasswrod = "";
-        returnpasswrod = password + salt;
-        return returnpasswrod;
-    }
+    private char[] hexDigits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
     /**
      * md5加密
@@ -32,25 +20,23 @@ public class DoMatch {
      * @return
      */
     public String Md5(String password, String salt, int time) {
-        String returnpasswrod = "";
-        returnpasswrod = password + salt;
+        String returnpasswrod = password + salt;
         for (int i = 0; i < time; i++) {
-            returnpasswrod = getmd5(returnpasswrod);
+            returnpasswrod = getMd5(returnpasswrod);
         }
         return returnpasswrod;
     }
 
-    public String  getmd5(String s) {
+    public String getMd5(String s) {
         try {
             byte[] strTemp = s.getBytes();
             MessageDigest mdTemp = MessageDigest.getInstance("MD5");
             mdTemp.update(strTemp);
             byte[] md = mdTemp.digest();
             int j = md.length;
-            char str[] = new char[j * 2];
+            char[] str = new char[j * 2];
             int k = 0;
-            for (int i = 0; i < j; i++) {
-                byte byte0 = md[i];
+            for (byte byte0 : md) {
                 str[k++] = hexDigits[byte0 >>> 4 & 0xf];
                 str[k++] = hexDigits[byte0 & 0xf];
             }
@@ -59,10 +45,5 @@ public class DoMatch {
             e.printStackTrace();
             return "";
         }
-    }
-
-    public static void main(String[] args) {
-        DoMatch doMatch=new DoMatch();
-        System.out.println(doMatch.Md5("123", "test", 2));
     }
 }
